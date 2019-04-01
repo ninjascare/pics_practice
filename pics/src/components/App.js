e5228326c5a6bd4723d09525774a29cda77bf050
ninjascare <you@example.com>
2019-04-01 14:29:20 -0400

    using axios to get data from the unsplash api

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class componentName extends Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 719f94ffaa69be7362d8c5cd6289dbd317441a8a20e95acffd1927a2bb0ee091"
+      }
+    });
   }
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
