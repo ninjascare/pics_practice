d245860bc77a512d46e1eef3caaa46bb2a8722a4
ninjascare <you@example.com>
2019-04-01 15:08:08 -0400

    added in a setState to update my state with the images that the api requested

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,7 +3,11 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class componentName extends Component {
-  async onSearchSubmit(term) {
+  state = {
+    images: []
+  };
+
+  onSearchSubmit = async term => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
@@ -11,13 +15,16 @@ class componentName extends Component {
           "Client-ID 719f94ffaa69be7362d8c5cd6289dbd317441a8a20e95acffd1927a2bb0ee091"
       }
     });
-    console.log(response.data.results);
-  }
+    this.setState({
+      images: response.data.results
+    });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} Images
       </div>
     );
   }
